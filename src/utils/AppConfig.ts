65d142d9226c18b44becb3f846811b0d7a623299
houssem.ammar.06@gmail.com <houssem.ammar.06@gmail.com>
2022-12-13 18:04:30 +0100

    project setup

---
+++ src/utils/AppConfig.ts
@@ -0,0 +1,8 @@
+// FIXME: Update this configuration file based on your project information
+
+export const AppConfig = {
+  site_name: 'c4t',
+  title: 'react test',
+  description: 'test with next js',
+  locale: 'en',
+};
