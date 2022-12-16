295132f84faaa81028134170aaecae4965e49596
houssem.ammar.06@gmail.com <houssem.ammar.06@gmail.com>
2022-12-16 21:35:19 +0100

    CRI-6-Implement-filtering-by-category

--- src/components/movies-list.tsx
+++ src/components/movies-list.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import MovieCard from '@/components/movie-card';
 import type { Movie } from '@/types/movies.types';
@@ -7,13 +7,42 @@ const MoviesList: React.FC<{ title: string; movies: Movie[] }> = ({
   title,
   movies,
 }) => {
+  const [category, setCategory] = useState('');
+  const [categoryList, setCategoryList] = useState([]);
+
+  useEffect(() => {
+    const existingCategories = movies.map((movie: any) => movie.category);
+    const categories: any = existingCategories.filter(
+      (element: any, index: number) => {
+        return existingCategories.indexOf(element) === index;
+      }
+    );
+    setCategoryList(categories);
+  }, [categoryList]);
   return (
     <div className="bg-darkPurple-800  shadow">
       <h1>{title}</h1>
-      <div className="flex flex-wrap">
-        {movies.map((movie: Movie) => (
-          <MovieCard movie={movie} />
+      <select
+        className=" my-36 ml-24 w-full"
+        onChange={(e) => {
+          setCategory(e.target.value);
+        }}
+      >
+        <option className="text-black" value={''}>
+          select category
+        </option>
+        {categoryList.map((el: string, index: number) => (
+          <option key={index} className="text-black" value={el}>
+            {el}
+          </option>
         ))}
+      </select>
+      <div className="flex flex-wrap">
+        {movies
+          .filter((el) => (category === '' ? el : el.category === category))
+          .map((movie: Movie) => (
+            <MovieCard key={movie.id} movie={movie} />
+          ))}
       </div>
     </div>
   );
